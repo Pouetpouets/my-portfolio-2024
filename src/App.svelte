6df317cc405807796bf0dd1fdc5d6706707926f1
Lucas Legrand <legrand.lucas0@gmail.com>
2024-12-21 18:18:19 +0100

    Update App.svelte to use new Contact component

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,20 @@
 <script lang="ts">
-  import ThreeScene from './lib/ThreeScene.svelte';
   import Navigation from './lib/Navigation.svelte';
   import Projects from './lib/Projects.svelte';
   import FunFacts from './lib/FunFacts.svelte';
+  import Contact from './lib/Contact.svelte';
 </script>
 
 <main>
   <Navigation />
-  <div class="scene-wrapper">
-    <ThreeScene />
-  </div>
   <Projects />
   <FunFacts />
+  <Contact />
 </main>
 
 <style>
   main {
     width: 100vw;
     background: #000;
-  }
-
-  .scene-wrapper {
-    height: 100vh;
-    overflow: hidden;
   }
 </style>
